b11a8aeccb7ebd013c15ba16e9a252d6ec2550ce
jche <jonche10@gmail.com>
2021-07-02 16:15:52 -0400

    some tweaks

--- example_basic.R
+++ example_basic.R
@@ -1,18 +1,22 @@
 
+#####
 # basic example: 1-site RCT
-
-# Power: P(detect effect | true nonzero effect)
-# MDE:   if I want 80% power, how big does the effect have to be?
-
+#
+# Power: if there is a nonzero effect of size x, what is my chance of detecting it?
+# MDE: if I want 80% power, how big does the effect have to be?
+#
 # 4 pieces of any power analysis => What to do for MDE:
+#
 #  1) effect size (standardized) => find this
 #  2) sample size:               => vary this
-#  3) significance level         => 0.05
-#  4) power level                => 0.80
+#  3) significance level         => set to 0.05
+#  4) power level                => set to 0.80
+#####
 
 require(tidyverse)
+require(glue)
 
-# f: (# obs, effect size) => (reject null? T/F)
+# function: (# obs, effect size) => (reject null? T/F)
 one_sim <- function(n, tau) {
   # generate data: constant treatment effect
   #  - note: sd=1, so TAU is already in Cohen's D units
@@ -22,12 +26,12 @@ one_sim <- function(n, tau) {
     yobs = y0 + tau*tx
   )
   
-  # compute t test
+  # compute t-test
   lm <- lm(yobs ~ tx, data=dat)
   summary(lm)$coefficients["tx",4] < 0.05
 }
 
-# f: (# obs, effect size) => (power)
+# function: (# obs, effect size) => (power)
 power_sim <- function(n, tau, NUMSIM = 250) {
   cat(glue("Working on n = {n}, tau = {tau}
 
@@ -37,7 +41,10 @@ power_sim <- function(n, tau, NUMSIM = 250) {
     mean()
 }
 
-# run power simulation
+# run power simulation:
+#  - n = number of observations
+#  - tau = true effect size
+#  - (real power sim would have more knobs: J, site.size, ICC/variances, etc.)
 df_sim <- expand_grid(
   n = c(25, 50, 75, 100),
   tau = c(0.01, 0.2, 0.5, 0.8)
@@ -54,12 +61,16 @@ ggplot(df_sim_res, aes(x=n, y=power, color=tau)) +
   coord_cartesian(ylim=c(0,1))
 
 
-# the real power analysis has many more knobs for the DGP (this one only has n and tau):
-#  - J, site.size (or R^2?), ICC (or variances?)
+# Note on "effect size" when we have an effect distribution:
+#  - each site has a (random) effect size
+#     - the simple idea is to aggregate "effect sizes" across simulations 
+#       (within effect-size bins) to compute power for that effect-size bin
 #
-# Q: what is an "effect size" when we have an effect distribution?
-#  - each site has an effect size, but detection of an effect for each site
-#    also depends on all of the other sites!
-#  - so just having averaging over all sites with the same "effect size" (across simulations)
-#    doesn't tell the whole story!
-# 
+#  - wrinkle: detection of an effect for each site *technically* depends on all of 
+#    the other sites in a given simulation (via the s.e. estimate)
+#     - so just averaging all sites within the same effect-size bin (across simulations) 
+#       doesn't tell the whole story, but it's close enough, I think
+
+
+
+
