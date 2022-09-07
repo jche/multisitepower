3b25bb0e1cffd4941bf70875385b0ceebd9ddf00
jche <jonche10@gmail.com>
2022-09-07 16:26:01 -0400

    tweaks, before major big sim with lots of sample sizes!

--- final_visualizations.R
+++ final_visualizations.R
@@ -8,18 +8,19 @@ tidy_results <- read_csv("tidy_results.csv")
 tidy_results <- tidy_results %>%
   mutate(reject_one = q10 >= 0,
          covered_one = q10 <= ATE,
+         reject_two = q5 >= ATE | q95 <= ATE,
          covered_two = (q5 <= ATE) & (ATE <= q95)) %>%
   mutate(ATEhat_bin = plyr::round_any(ATEhat, 0.05))
 
 
 
 
-# INCLUDE_OVERALL <- F   # include overall estimators?
 TAU_FIXED <- 0.2
 J_FIXED <- 100
 
 # firc1 is using se.fixef + se.ranef, firc2 uses arm() samples
 tidy_results_sm <- tidy_results %>%
+  filter(J == J_FIXED, tau == TAU_FIXED) %>%
   filter(method %in% c("firc2", "rirc2", "bayesnorm", "single")) %>%
   mutate(method = case_when(
     method == "firc2" ~ "FIRC",
@@ -29,21 +30,19 @@ tidy_results_sm <- tidy_results %>%
 
 
 
-# coverage/power ----------------------------------------------------------
+# coverage/power, one-sided ------------------------------------------------
 
 MIN_SIZE <- 50
 
 # prep power df
-power_df <- tidy_results %>% 
-  {if (!INCLUDE_OVERALL) filter(., !str_detect(method, "_overall")) else .} %>%
+power_df <- tidy_results_sm %>% 
   group_by(nbar, tx_sd, tau, J, method, ATE) %>%
   summarize(n = n(),
             power = mean(reject_one)) %>%
   filter(n >= MIN_SIZE)
 
 # prep coverage dfs
-coverage1_df <- tidy_results %>% 
-  {if (!INCLUDE_OVERALL) filter(., !str_detect(method, "_overall")) else .} %>%
+coverage1_df <- tidy_results_sm %>% 
   group_by(nbar, tx_sd, tau, J, method, ATE) %>%
   summarize(n = n(),
             coverage_one = mean(covered_one)) %>%
@@ -51,13 +50,6 @@ coverage1_df <- tidy_results %>%
 
 # plot one-sided interval coverage vs. ATE size
 coverage1_df %>%
-  filter(J == J_FIXED, tau == TAU_FIXED) %>%
-  filter(method %in% c("firc2", "rirc2", "bayesnorm", "single")) %>%
-  mutate(method = case_when(
-    method == "firc2" ~ "FIRC",
-    method == "rirc2" ~ "RIRC",
-    method == "bayesnorm" ~ "Bayes",
-    method == "single" ~ "Single")) %>%
   ggplot() +
   geom_line(aes(x=ATE, y=coverage_one, color=method, group=method)) +
   facet_grid(tx_sd ~ nbar, labeller=label_both) +
@@ -73,14 +65,6 @@ ggsave(glue("writeup/images/coverage_plot.png"), width=200, height=125, units="m
 # Plot power vs. ATE size
 # I UPDATED THIS
 power_df %>%
-  filter(J == J_FIXED, tau == TAU_FIXED) %>%
-  filter(method %in% c("firc2", "rirc2", "bayesnorm", "single")) %>%
-  mutate(method = case_when(
-    method == "firc2" ~ "FIRC",
-    method == "rirc2" ~ "RIRC",
-    method == "bayesnorm" ~ "Bayes",
-    method == "single" ~ "Single")) %>%
-  
   ggplot(aes(x = ATE, y = power, col = method, group=method ) ) +
   facet_grid(tx_sd ~ nbar, labeller=label_both ) +
   geom_line(alpha = 0.7) +
@@ -97,17 +81,117 @@ power_df %>%
 ggsave(glue("writeup/images/power_plot_J100.png"), width=200, height=125, units="mm")
 
 
-# interval length ---------------------------------------------------------
+
+# coverage/power, two-sided -----------------------------------------------
+
+MIN_SIZE <- 50
+
+# prep power df
+power_df2 <- tidy_results_sm %>% 
+  group_by(nbar, tx_sd, tau, J, method, ATE) %>%
+  summarize(n = n(),
+            power = mean(reject_two)) %>%
+  filter(n >= MIN_SIZE)
+
+# prep coverage dfs
+coverage_df2 <- tidy_results_sm %>% 
+  group_by(nbar, tx_sd, tau, J, method, ATE) %>%
+  summarize(n = n(),
+            coverage_two = mean(covered_two)) %>%
+  filter(n >= MIN_SIZE)
+
+# plot one-sided interval coverage vs. ATE size
+coverage_df2 %>%
+  ggplot() +
+  geom_line(aes(x=ATE, y=coverage_two, color=method, group=method)) +
+  facet_grid(tx_sd ~ nbar, labeller=label_both) +
+  geom_hline(yintercept=0.9, lty="dashed") +
+  labs(title = "Conditional coverage rates (two-sided CI)",
+       subtitle = glue("J = {J_FIXED}, \u03C4 = {TAU_FIXED}"),
+       y = "Coverage",
+       x = "True site ATE",
+       color = "Method") +
+  theme_classic()
+# ggsave(glue("writeup/images/coverage_plot2.png"), width=200, height=125, units="mm")
+
+# Plot power vs. ATE size
+power_df2 %>%
+  ggplot(aes(x = ATE, y = power, col = method, group=method ) ) +
+  facet_grid(tx_sd ~ nbar, labeller=label_both ) +
+  geom_line(alpha = 0.7) +
+  # geom_hline( yintercept = 0.8, lty = "dashed" ) +
+  geom_hline( yintercept = 0.1, lty = "dashed" ) +
+  geom_vline( xintercept = 0 ) +
+  coord_cartesian(xlim = c(-0.5, 0.6)) +
+  labs(title = glue("Power (\u03B1 = 0.1) vs. true site effect (two-sided CI)"),
+       subtitle = glue("J = {J_FIXED}, \u03C4 = {TAU_FIXED}"),
+       y = "Power",
+       x = "Site-level ATE",
+       color = "Method") +
+  theme_classic()
+# ggsave(glue("writeup/images/power_plot2.png"), width=200, height=125, units="mm")
+
+
+
+# what if we use ATEhat_bin instead? --------------------------------------
+
+MIN_SIZE <- 50
+
+# prep power df
+power_df2hat <- tidy_results_sm %>% 
+  group_by(nbar, tx_sd, tau, J, method, ATEhat_bin) %>%
+  summarize(n = n(),
+            power = mean(reject_two)) %>%
+  filter(n >= MIN_SIZE)
+
+# prep coverage dfs
+coverage_df2hat <- tidy_results_sm %>% 
+  group_by(nbar, tx_sd, tau, J, method, ATEhat_bin) %>%
+  summarize(n = n(),
+            coverage_two = mean(covered_two)) %>%
+  filter(n >= MIN_SIZE)
+
+# plot two-sided interval coverage vs. ATE size
+coverage_df2hat %>%
+  ggplot() +
+  geom_line(aes(x=ATEhat_bin, y=coverage_two, color=method, group=method)) +
+  facet_grid(tx_sd ~ nbar, labeller=label_both) +
+  geom_hline(yintercept=0.9, lty="dashed") +
+  labs(title = "Conditional coverage rates (two-sided CI)",
+       subtitle = glue("J = {J_FIXED}, \u03C4 = {TAU_FIXED}"),
+       y = "Coverage",
+       x = "Estimated site ATE",
+       color = "Method") +
+  theme_classic()
+# ggsave(glue("writeup/images/coverage_plot2.png"), width=200, height=125, units="mm")
+
+# Plot power vs. ATE size
+power_df2hat %>%
+  ggplot(aes(x = ATEhat_bin, y = power, col = method, group=method ) ) +
+  facet_grid(tx_sd ~ nbar, labeller=label_both ) +
+  geom_line(alpha = 0.7) +
+  # geom_hline( yintercept = 0.8, lty = "dashed" ) +
+  geom_hline( yintercept = 0.1, lty = "dashed" ) +
+  geom_vline( xintercept = 0 ) +
+  coord_cartesian(xlim = c(-0.5, 0.6)) +
+  labs(title = glue("Power (\u03B1 = 0.1) vs. true site effect (two-sided CI)"),
+       subtitle = glue("J = {J_FIXED}, \u03C4 = {TAU_FIXED}"),
+       y = "Power",
+       x = "Site-level ATE",
+       color = "Method") +
+  theme_classic()
+
+
+# interval length, two-sided -----------------------------------------------
 
 tidy_results_sm %>%
-  filter(J == J_FIXED, tau == TAU_FIXED) %>%
   mutate(length = q95 - q5) %>%
   group_by(nbar, J, ICC, tau, tx_sd, method) %>%
   summarize(mn_length = mean(length)) %>%
   ggplot(aes(x=nbar, y=mn_length, color=method, group=method)) +
   geom_point() +
   geom_line() +
-  facet_grid(~tx_sd) +
+  facet_grid(~tx_sd, labeller="label_both") +
   labs(title = "Average interval lengths (two-sided CI)",
        subtitle = glue("J = {J_FIXED}, \u03C4 = {TAU_FIXED}"),
        y = "Average interval length",
